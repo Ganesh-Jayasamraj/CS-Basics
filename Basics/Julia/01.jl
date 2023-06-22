7d765ce60f37ed42797fad9b1c1efc7c17136f81
Ganesh-Jayasamraj <ganesh.jayasamraj@gmail.com>
2023-06-22 15:16:36 +0530

    Started with Julia

---
+++ Basics/Julia/01.jl
@@ -0,0 +1,12 @@
+# Variables
+students_name = "Ram"
+roll_no = 15
+maths_mark = 9.0
+
+# Array
+arr = [1, 2, 3, 4, 5]
+
+# Uninitialinzed Array
+array = Array{Int64}(undef, 3)
+
+array = Array{Int64}(undef, 3, 3, 3)
