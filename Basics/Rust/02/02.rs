875a10307213e6c93d16a11e30ff8c4ca88d17e6
GANESH JAYASAMRAJ <ganesh.jayasamraj@gmail.com>
2023-07-06 13:06:03 +0530

    Starting JS

---
+++ Basics/Rust/02/02.rs
@@ -0,0 +1,13 @@
+fn main() {
+    const A:i32 = 108;
+    const B:f32 = 56.89;
+
+    println!("{} {}", A, B);
+
+    // Constants are immutable in nature
+
+        // A = 98;
+        // B = 78.68;
+        // println!("{} {}", A, B);
+        
+}
