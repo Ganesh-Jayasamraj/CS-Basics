e040f3ef440ef8484e412a26d3ef64fa56f28cd2
GANESH JAYASAMRAJ <ganesh.jayasamraj@gmail.com>
2023-07-06 12:36:20 +0530

    nothing

--- Basics/Rust/01/01.rs
+++ Basics/Rust/01/01.rs
@@ -1,4 +1,27 @@
+// Variables
+
 fn main() {
-    println!("Hello World!")
+
+    // Hello World
+
+    println!("Hello World!");
+
+    // Declare Variable
+    // Variables must begin with letters and underscore "_"
+
+    let a = "Some text";
+    let b = 4.12;           // Some float
+    let c = true;           // Boolean
+    let d = "❤";           // Unicode character
+    println!("{} {} {} {}", a, b, c, d);
+
+    // By default Variables are immutable
+        /* 
+            let test_var = 12345;
+            println!("{}", test_var);
+
+            test_var = 123123;
+            println!("{}", test_var);
+        */
 }
 
